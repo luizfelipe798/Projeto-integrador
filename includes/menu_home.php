7f1e5c3a75fc67284cef3a6d859211a3f22af16b
luizfelipe798 <luizvignoto@gmail.com>
2025-10-14 12:52:34 -0300

    Divisão dos menus em dois arquivos diferentes na pasta "includes"

--- includes/menu_home.php
+++ includes/menu_home.php
@@ -2,22 +2,6 @@
     session_start();
 ?>
 
-<?php if(!isset($_SESSION['logado']) || $_SESSION['logado'] !== "Sim"):?>
-<menu class="menuPrincipal">
-        <div class="logotipoLinks">
-                <a href="index.php" class="logotipoMenu"><img class="logotipoMenu" src="imagens/logotiposalus.png" alt="Logotipo Sailus"></a>
-                <a href="#menuPrincipal">Início</a>
-                <a href="#sobrenosBody">Sobre nós</a>
-                <a href="#desenvolvedoresBody">Desenvolvedores</a>
-                <a href="#footer">Instituição</a>
-        </div>
-
-        <div class="botoesCadastroLogin">
-            <a href="paginas/login.php" class="btnLogin">Login</a>
-            <a href="paginas/escolha.html" class="btnCadastro">Cadastre-se</a>
-        </div>
-</menu>
-<?php else:?>
 <menu class="menuPrincipal">
         <div class="logotipoLinks">
                 <a href="home_usuario.php" class="logotipoMenu"><img class="logotipoMenu" src="../imagens/logotiposalus.png" alt="Logotipo Sailus"></a>
@@ -34,4 +18,3 @@
             <a href=""><img src="../imagens/perfil_usuario.png" alt="Perfil do usuário"></a>
         </div>
 </menu>
-<?php endif; ?>
