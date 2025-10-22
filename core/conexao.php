e2e773d356822f9f46252dc83a03e961a90892ec
luizfelipe798 <luizvignoto@gmail.com>
2025-10-22 16:25:26 -0300

    Mudança na estrutura do arquivo que realiza a conexão com o banco de dados

--- core/conexao.php
+++ core/conexao.php
@@ -1,13 +1,27 @@
 <?php
-    $bdNome = "sailus";
-    $bdServidor = "localhost";
-    $bdUsuario = "root";
-    $bdSenha = "815674815";
-
-    $conexao = new mysqli($bdServidor, $bdUsuario, $bdSenha, $bdNome);
-
-    if($conexao->connect_error)
+    function conecta() : mysqli
     {
-        echo "Erro de conexão: " . $conexao->connect_error;
+        $servidor = 'localhost';
+        $banco = 'blog';
+        $port = 3306;
+        $usuario = 'root';
+        $senha = '815674815';
+        
+        $conexao = mysqli_connect($servidor, $usuario, $senha, $banco, $port);
+
+        if(!$conexao)
+        {
+            echo 'Erro: Não foi possível conectar ao MySql.' . PHP_EOL;
+            echo 'Debugging errno: ' . mysqli_connect_errno() . PHP_EOL;
+            echo 'Debugging error: ' . mysqli_connect_error() . PHP_EOL;
+            return null;
+        }
+        
+        return $conexao;
+    }
+
+    function desconecta($conexao)
+    {
+        mysqli_close($conexao);
     }
 ?>
