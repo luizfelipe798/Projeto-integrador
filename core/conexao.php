70acc918b54faebbd38a07678c4959e8456e2080
luizfelipe798 <luizvignoto@gmail.com>
2025-10-22 16:25:50 -0300

    Uma linha estava faltando

--- core/conexao.php
+++ core/conexao.php
@@ -6,7 +6,7 @@
         $port = 3306;
         $usuario = 'root';
         $senha = '815674815';
-        
+
         $conexao = mysqli_connect($servidor, $usuario, $senha, $banco, $port);
 
         if(!$conexao)
@@ -14,6 +14,7 @@
             echo 'Erro: Não foi possível conectar ao MySql.' . PHP_EOL;
             echo 'Debugging errno: ' . mysqli_connect_errno() . PHP_EOL;
             echo 'Debugging error: ' . mysqli_connect_error() . PHP_EOL;
+            
             return null;
         }
         
